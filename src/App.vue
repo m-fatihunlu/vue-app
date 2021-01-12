1a77f55116fb5f20f146e338b721e74e8a0cf10c
Fatih Ünlü <fatih.unlu@multinetinventiv.com>
2021-01-12 17:42:18 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-
+      Selam herkese .. 
     <HelloWorld msg="Welcome to Your Vue.js App GUYS"/>
     <HelloWorld msg="Welcome to Your Vue.js App GUYS"/>
     <HelloWorld msg="Welcome to Your Vue.js App GUYS"/>
